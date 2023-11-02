64af9d0ff0759cee37f9ea5279a2347ff530bbf6
Netcode-Hub <Rastinga1995@hotmail.com>
2023-11-02 10:29:16 +0000

    Calling A Method In A Child Component using @Ref Attribute Or Event CallBack Method in Blazor App.

---
+++ DemoRaisingMethodsWithRefAttributeAndEventCallBack/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoRaisingMethodsWithRefAttributeAndEventCallBack
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
